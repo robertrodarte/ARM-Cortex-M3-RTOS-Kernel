b01ad7859905349fb968d0ace1d08a96b8214ee5
Robert <robertrodarte02@gmail.com>
2026-09-06 23:28:42 -0700

    Building small main function to test initial linker.ld and startup.S

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/* Used to test zeroed bss and copied data */
+int uninitGlobal;     // Define an uninitialized global variable (.bss)
+int initGlobal = 115; // Define a global variable (.data)
+
+int main()
+{
+    // Infinite loop
+    while (1)
+    {
+        // Logic goes here
+    }
+
+    return 0;
+}
