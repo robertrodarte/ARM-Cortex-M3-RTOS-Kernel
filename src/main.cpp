516d7924ad6c2eae9a34fbcfd44b382b989cd179
Robert <robertrodarte02@gmail.com>
2026-09-07 01:29:50 -0700

    Adding static keyword to global vars

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /* Used to test zeroed bss and copied data */
-int uninitGlobal;     // Define an uninitialized global variable (.bss)
-int initGlobal = 115; // Define a global variable (.data)
+static int uninitGlobal;     // Define an uninitialized global variable (.bss)
+static int initGlobal = 115; // Define a global variable (.data)
 
 int main()
 {
